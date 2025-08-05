8fd35d253c1d0adf90ce165d0f586d2f1d9fed1e
kzijm <Neok_z@hotmail.com>
2025-08-05 12:24:50 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '00.wishlist_ang20_2';
+}
